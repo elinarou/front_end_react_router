e06a09008dc09a024ea5a90b2817d0f848a7e56f
elinarou <elina_rouvinen@outlook.com>
2022-10-13 13:04:52 +0300

    Tehtava valmis

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
+
+  function Home() {
+    return (<h1>This is home page</h1>);
+  };
+
+  function About() {
+    return (<h1>This is about page</h1>);
+  };
+
+  function Contact() {
+    return (<h1>This is contact page</h1>);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <h1>Welcome to React Router</h1>
       </header>
+      <body>
+        <BrowserRouter>
+          <Link to="/">Home</Link>{' '}
+          <Link to="/about">About</Link>{' '}
+          <Link to="/contact">Contact</Link>{' '}
+          <Routes>
+            <Route exact path="/" element={<Home />} /> 
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} /> 
+          </Routes>
+        </BrowserRouter>
+      </body>
     </div>
   );
 }
